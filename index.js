cc990b9905b26bca0f2c28f036c198c604e8d002
agusydk3 <57493908+agusydk3@users.noreply.github.com>
2020-10-01 21:03:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,55 +4,36 @@ const options = require('./utils/options')
 const msgHandler = require('./handler/message')
 
 const start = (client = new Client()) => {
-    console.log('[DEV]', color('Red Emperor', 'yellow'))
-    console.log('[CLIENT] CLIENT Started!')
+    console.log('[DEV]', color('Slavyan', 'orange')) // Change your name and color here
+    console.log('[CLIENT] Bot is now online!')
 
     // Force it to keep the current session
     client.onStateChanged((state) => {
         console.log('[Client State]', state)
+        if (state === 'UNPAIRED') client.forceRefocus()
         if (state === 'CONFLICT') client.forceRefocus()
     })
 
-    // listening on message
+    // Set all received message to seen
+    client.onAck((x => {
+        const { to } = x
+        if (x !== 3) client.sendSeen(to)
+    }))
+
+    // Listening on message
     client.onMessage((message) => {
-        client.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
+        client.getAmountOfLoadedMessages() // Cut message cache if it reach more than 3K
             .then((msg) => {
                 if (msg >= 3000) {
-                    console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    console.log('[CLIENT]', color(`Loaded message reach ${msg}, cuting message cache...`, 'yellow'))
                     client.cutMsgCache()
                 }
             })
-        // Message Handler
-        msgHandler(client, message)
-    })
-
-    // listen group invitation
-    client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) =>
-        client.getGroupMembersId(id)
-            .then((ids) => {
-                console.log('[CLIENT]', color(`Invited to Group. [ ${name} : ${ids.length}]`, 'yellow'))
-                // conditions if the group members are less than 10 then the bot will leave the group
-                if (ids.length <= 10) {
-                    client.sendText(id, 'Sorry, the minimum group member is 10 user to use this bot. Bye~').then(() => client.leaveGroup(id))
-                } else {
-                    client.sendText(id, `Hello group members *${name}*, thank you for inviting this bot, to see the bot menu send *#menu*`)
-                }
-            }))
-
-    client.onRemovedFromGroup((data) => {
-        // console.log(data)
-    })
-
-    // listen paricipant event on group (wellcome message)
-    client.onGlobalParicipantsChanged((event) => {
-        // if (event.action === 'add') client.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
-    })
-
-    client.onIncomingCall((callData) => {
-        // client.contactBlock(callData.peerJid)
+        msgHandler(client, message) // Message handler
     })
 }
 
-create('Imperial', options(true, start))
+// Creating Slavyan.data.json
+create('Slavyan', options(true, start)) // Change your session name here
     .then((client) => start(client))
     .catch((err) => new Error(err))
